c04229e7e3929df89e377e6ffdcfd58432336dd4
viktor <mr-zjops19882007@yandex.ru>
2023-12-13 22:17:32 +0300

    fix

--- config/logging.php
+++ config/logging.php
@@ -131,8 +131,8 @@ return [
             'driver' => 'custom',
             'via' => \App\Logging\Telegram\TelegramLoggerFactory::class,
             'level' => env('LOG_LEVEL', 'debug'),
-            'chat_id' => env('TELEGRAM_CHAT_ID'),
-            'token' => env('TELEGRAM_TOKEN'),
+            'chat_id' => env('LOGGER_TELEGRAM_BOT_CHAT_ID'),
+            'token' => env('LOGGER_TELEGRAM_BOT_TOKEN'),
         ],
     ],
 
