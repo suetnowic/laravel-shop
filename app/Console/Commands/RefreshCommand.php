db63096a290a8ca66e3ba95208b83e6b3e92ef1e
viktor <mr-zjops19882007@yandex.ru>
2023-12-16 21:12:00 +0300

    the removal of the products catalog in app/public/images has been added to the shop:refresh console command

--- app/Console/Commands/RefreshCommand.php
+++ app/Console/Commands/RefreshCommand.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class RefreshCommand extends Command
 {
@@ -16,6 +17,7 @@ class RefreshCommand extends Command
         if (app()->isProduction()) {
             return self::FAILURE;
         }
+        Storage::deleteDirectory('images/products');
         $this->call('migrate:fresh', ['--seed' => true]);
         return self::SUCCESS;
     }
