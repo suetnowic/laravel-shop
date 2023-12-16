5e30ade7e1f56551ec7a439bad462d38cc3dad4c
viktor <mr-zjops19882007@yandex.ru>
2023-12-16 19:12:21 +0300

    added a console command for migrate:fresh --seed

---
+++ app/Console/Commands/RefreshCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RefreshCommand extends Command
+{
+
+    protected $signature = 'shop:refresh';
+
+    protected $description = 'Refresh';
+
+    public function handle(): int
+    {
+        if (app()->isProduction()) {
+            return self::FAILURE;
+        }
+        $this->call('migrate:fresh', ['--seed' => true]);
+        return self::SUCCESS;
+    }
+}
